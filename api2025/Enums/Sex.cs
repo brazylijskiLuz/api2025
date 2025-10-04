ea3354dd55d3f0b6a5d2a84fd749d2a96894b115
Jan Rzepisko <60238932+JanRzepisko@users.noreply.github.com>
2025-10-04 20:50:00 +0200

    Fix enumeration

--- api2025/Enums/Sex.cs
+++ api2025/Enums/Sex.cs
@@ -5,7 +5,7 @@ namespace api2025.Enums;
 public class Sex : Enumeration
 {
     public static Sex Male = new Sex(1, "Męszczyzna");
-    public static Sex Female = new Sex(1, "Kobieta");
+    public static Sex Female = new Sex(2, "Kobieta");
 
     public Sex(int id) : base(id)
     {
